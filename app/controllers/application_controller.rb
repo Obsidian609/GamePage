dc0044e136103667b8ffbb72757163faa21b156c
obsidian609 <obsidian0504@gmail.com>
2020-10-20 04:04:07 -0400

    added token whenever a user registers. added an authentication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,3 +11,17 @@ end
         HashWithIndifferentAccess.new decoded
     end
 end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            @token = encoded({id: @user.id})
+            render json: {
+                user: @user.attributes.except(:password_digest),
+                token: @token
+            }, status: :created
+        else
+            render json: @user.errors, status: unprocessable_entity
+        end
+    end
