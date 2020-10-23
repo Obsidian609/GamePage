a8e20ea5f217c9f063ad8261c6f605bff4cf2971
obsidian609 <obsidian0504@gmail.com>
2020-10-23 04:47:09 -0400

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@user = User.create(username: 'admin', email:'admin@email.com', password: '123456')
+
+@game = Game.create(name: 'Dead Space', user: @user)
+
+@comment = Comment.create(name:"Can't wait for the remakes! 10/10 would play again.", user: @user)
