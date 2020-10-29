f8fd0a90cd1b28b8d66a592f27cd553b780bc6c1
obsidian609 <obsidian0504@gmail.com>
2020-10-28 21:11:54 -0400

    added dependecies needed to deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@ User.destroy_all
 @user = User.create(username: 'admin', email:'admin@email.com', password: '123456')
 
 @game = Game.create(name: 'Dead Space', image: 'https://upload.wikimedia.org/wikipedia/en/5/57/Dead_Space_Box_Art.jpg', user: @user)
+
+comment1 = Comment.create(name: 'Dead Space has 3 games out now', user_id: @user.id game_id: @game.id)
+comment2 = Comment.create()
+comment3 = Comment.create()
+comment4 = Comment.create()
+comment5 = Comment.create()
