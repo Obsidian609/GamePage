3c2c67e2412de689915bda625abd4cda78c7fb1c
obsidian609 <obsidian0504@gmail.com>
2020-10-29 10:56:37 -0400

    fixed api-config heroku link

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://git.heroku.com/gamepage-api.git' : "http://localhost:3000";
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://gamepage-api.herokuapp.com/' : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseURL,
